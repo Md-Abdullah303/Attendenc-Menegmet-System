4f88f8b54b1c1d1e88e5c2667dc682c1a906fd51
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-04-19 22:59:51 +0600

    add a btn in students page

--- src/pages/StudentsPage/StudentsPage.jsx
+++ src/pages/StudentsPage/StudentsPage.jsx
@@ -1,23 +1,33 @@
-import React, { Suspense } from 'react';
-import { useLoaderData } from 'react-router';
-import StudentPageCardContainer from '../../component/StudentPageCardContainer/StudentPageCardContainer';
-import { ClockLoader } from 'react-spinners';
+import React, { Suspense } from "react";
+import { useLoaderData } from "react-router";
+import StudentPageCardContainer from "../../component/StudentPageCardContainer/StudentPageCardContainer";
+import { ClockLoader } from "react-spinners";
 
 const StudentsPage = () => {
-    const studentsData = useLoaderData();
-    return (
-        <div className='w-[90%] sm:container space-y-3.5  mx-auto pt-20 pb-10'>
-            <div className="flex items-center justify-between gap-5">
-                <h1 className='text-xl sm:text-3xl md:text-4xl font-bold italic text-green-950'>Our Students</h1>
-                <input className='bg-gray-200 py-2 px-4 mx-3 w-30 sm:w-[15vw] text-lg outline-none rounded-lg' type="text" placeholder='Search' />
-            </div>
-
-            <Suspense fallback={<ClockLoader />}>
-                <StudentPageCardContainer studentsData={studentsData}></StudentPageCardContainer>
-            </Suspense>
-
+  const studentsData = useLoaderData();
+  return (
+    <div className="w-[90%] sm:container space-y-3.5  mx-auto pt-20 pb-10">
+      <div className="flex items-center justify-between gap-5">
+        <h1 className="text-xl sm:text-3xl md:text-4xl font-bold italic text-green-950">
+          Our Students
+        </h1>
+        <div className="">
+          <input
+            className="bg-gray-200 py-2 px-4 mx-3 w-30 sm:w-[15vw] text-lg outline-none rounded-lg"
+            type="text"
+            placeholder="Search"
+          />
+          <button className="btn btn-primary">Search</button>
         </div>
-    );
+      </div>
+
+      <Suspense fallback={<ClockLoader />}>
+        <StudentPageCardContainer
+          studentsData={studentsData}
+        ></StudentPageCardContainer>
+      </Suspense>
+    </div>
+  );
 };
 
 export default StudentsPage;
